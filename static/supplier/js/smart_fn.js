f9b7a140beb9c7be0017274ec0e0bcdf054d5f96
Nuntapong Yamsang <n.yamsang@gmail.com>
2020-01-28 10:12:47 +0700

    Commit

--- static/supplier/js/smart_fn.js
+++ static/supplier/js/smart_fn.js
@@ -2,5 +2,21 @@ class SmartFn {
 	static initPage() {
 		console.log("fn: SmartFn.initPage");
 		$(document).attr("title", "Smart Restaurant - Supplier");
+		var current_date = new Date();
+		var str_date = current_date.getUTCFullYear()+"-"+SmartFn.pad(current_date.getMonth()+1, 2)+"-"+current_date.getDate();
+		$("#order_date").val(str_date);
+		$("#order_date").change(function () {
+			console.log("jq: #order_date.change = "+$("#order_date").val());
+		});
+	}
+	
+	static getOrderBatchList() {
+		console.log("fn: SmartFn.getOrderBatchList");
+	}
+	
+	static pad(n, width, z) {
+		z = z || '0';
+		n = n + '';
+		return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 	}
 }
